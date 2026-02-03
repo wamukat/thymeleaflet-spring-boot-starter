4e54c0cbe32ff5c01adc7ab88f374a526eb0da61
Takuma Watanabe <takuma.watanabe.yf@hitachi-solutions.com>
2026-02-04 08:40:04 +0900

    Fix i18n message loading and English bundle

--- src/main/java/io/github/wamukat/thymeleaflet/infrastructure/configuration/WebConfig.java
+++ src/main/java/io/github/wamukat/thymeleaflet/infrastructure/configuration/WebConfig.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    
+
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         // パスパラメータでドット(.)が切り捨てられないようにする
